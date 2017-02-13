ca9d6f2dd8fe494e26f6ceeec14079c055613b40
RuthlessLion <windowtheworldofgames@gmail.com>
2017-02-13 16:56:20 +0700

    End laba 2

--- dohod.cpp
+++ dohod.cpp
@@ -1,8 +1,42 @@
 #include <iostream>
 using namespace std;
 int main (){
-int  srok,summ,proc;
-cout << "Введите срок вклада:"
+int  srok,summ,proc,procend;
+cout << "Введите срок вклада:";
 cin >> srok;
-cout << srok;
+cout << "Введите сумму вклада:";
+cin >> summ;
+if (srok <= 30){
+	if(summ <= 100000){
+		proc = summ*0.9;
+	} else 
+	if(summ >100000){
+		proc = summ*0.9;
+	}
+}
+if (srok > 31 && srok <120){
+	if(summ <= 100000){
+		proc = summ*1.02;
+	} else 
+	if(summ >100000){
+		proc = summ*1.03;
+	}
+}
+if (srok >121 && srok < 240){
+	if(summ <= 100000){
+		proc = summ*1.06;
+	} else 
+	if(summ >100000){
+		proc = summ*1.08;
+	}
+}
+if (srok > 240 && srok <365){
+	if(summ <= 100000){
+		proc = summ*1.12;
+	} else 
+	if(summ >100000){
+		proc = summ*1.15;
+	}
+}
+cout << "Начисленные проценты" << proc;
 }
